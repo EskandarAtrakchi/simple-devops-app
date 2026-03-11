6d448c7c00e05748cdfb652989fc6b4cb7763abf
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2026-03-11 18:31:18 +0000

    Refactor app.js to improve security and error handling
    Removed trust proxy and HTTPS redirection middleware, added security headers with helmet, and updated error handling.

--- app.js
+++ app.js
@@ -3,25 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
-
-// trust proxy (required for Render HTTPS)
-app.set('trust proxy', 1);
-
-// force HTTPS
-app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect(`https://${req.headers.host}${req.url}`);
-  }
-  next();
-});
-
-// security headers
-app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -35,16 +20,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// catch 404
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
