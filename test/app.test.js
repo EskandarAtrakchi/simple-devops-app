48a5beb20efe1d1e8f3e86e98e7399e067826678
EskandarAtrakchi <eskandaratrakchi8@gmail.com>
2026-03-12 12:28:28 +0000

    testing fixed

---
+++ test/app.test.js
@@ -0,0 +1,15 @@
+const assert = require("assert");
+
+describe("Math tests", function () {
+
+  it("should return 5 when adding 2 + 3", function () {
+    const result = 2 + 3;
+    assert.equal(result, 5);
+  });
+
+  it("should return 8 when multiplying 4 * 2", function () {
+    const result = 4 * 2;
+    assert.equal(result, 8);
+  });
+
+});
